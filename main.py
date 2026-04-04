b159dde8804ffcd589a08e58df60ea03d4c549a4
datarthur <maillartarthur@email.com>
2026-04-04 16:40:32 +0200

    feat: fetch champion spells from Data Dragon

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import requests
 
-# Étape 1 : récupérer la dernière version
-versions = requests.get("https://ddragon.leagueoflegends.com/api/versions.json")
-# convertis en json, prends le premier élément de la liste -> c'est la dernière version
-last_version = versions.json()[0]
-# Étape 2 : construire l'URL des champions avec cette version
-champions = requests.get(f"https://ddragon.leagueoflegends.com/cdn/{last_version}/data/fr_FR/champion.json")
-# remplace VERSION par la variable que t'as récupérée (utilise un f-string)
 
-# Étape 3 : appeler cette URL et convertir en json
+versions = requests.get("https://ddragon.leagueoflegends.com/api/versions.json")
+
+last_version = versions.json()[0]
+
+champions = requests.get(f"https://ddragon.leagueoflegends.com/cdn/{last_version}/data/fr_FR/champion.json")
+
 champions = champions.json()
-# Étape 4 : boucler sur les champions et afficher leurs noms
-# les champions sont dans la clé "data" du json
+
 for element in champions["data"]:
     print(element)
+
+
+champion_detail = requests.get(f"https://ddragon.leagueoflegends.com/cdn/{last_version}/data/fr_FR/champion/Darius.json")
+champion_detail = champion_detail.json()
+
+spells = champion_detail["data"]["Darius"]["spells"]
+for spell in spells:
+    print(spell["name"])
